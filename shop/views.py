6dad63175b0275d95421f9c38b0c1b39c2078ae1
daliyev <daliyevazizbekk@gmail.com>
2024-03-30 10:51:36 +0500

    list and detail templates

--- shop/views.py
+++ shop/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render, get_object_or_404
 
 from .models import Category, Product
 
+
 def product_list(request, category_slug=None):
     category = None
     categories = Category.objects.all()
-    produscts = Product.objects.filter(available=True)
+    products = Product.objects.filter(available=True)
     if category_slug:
         category = get_object_or_404(Category, slug=category_slug)
         products = products.filter(category=category)
