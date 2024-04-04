ef0a4449a70fe87d64e7badcc65a7e32dd4c127a
daliyev <daliyevazizbekk@gmail.com>
2024-04-04 09:42:55 +0000

    add webhook to receive payment notification

--- orders/admin.py
+++ orders/admin.py
@@ -1,15 +1,21 @@
 from django.contrib import admin
-
 from .models import Order, OrderItem
-
+from django.utils.safestring import mark_safe
 
 class OrderItemInline(admin.TabularInline):
     model = OrderItem
     raw_id_fields = ['product']
 
-
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ['id', 'first_name', 'last_name', 'email', 'address', 'postal_code', 'city', 'paid', 'created', 'updated']
+    list_display = ['id', 'first_name', 'last_name', 'email', 'address', 'postal_code', 'city', 'paid', 'stripe_payment', 'created', 'updated']
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
+
+    def stripe_payment(self, obj):
+        url = obj.get_stripe_url()
+        if obj.stripe_id:
+            html = f'<a href="{url}" target="_blank">{obj.stripe_id}</a>'
+            return mark_safe(html)
+        return ''
+    stripe_payment.short_description = 'Stripe payment'
--- payment/urls.py
+++ payment/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from . import webhook
 
 app_name = 'payment'
 
@@ -7,4 +8,8 @@ urlpatterns = [
     path('process/', views.payment_process, name='process'),
     path('completed/', views.payment_completed, name='completed'),
     path('canceled/', views.payment_canceled, name='canceled'),
+    path('webhook/', webhook.stripe_webhook, name='stripe-webhook'),
 ]
+
+
+# https://www.c-sharpcorner.com/article/how-to-schedule-automatic-shut-down-in-windows-10/
